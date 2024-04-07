7b4c5466fc30ebbd15157d91f0728429207e1df7
Flavio Luz <frpluz@gmail.com>
2024-04-07 03:05:23 -0400

    Validation du login et du mot de passe

--- js/login.js
+++ js/login.js
@@ -1,52 +1,81 @@
-function onChangeEmail() {
-    basculeBoutonsDesactives();
+var listeUtilisateurs;
+
+function valideLogin() {
+    chargerUtilisateurs();
     basculeErreursEmail();
-}
-
-function onChangeMotPasse() {
-    basculeBoutonsDesactives();
     basculeErreursMotPasse()
-}
-
-function login() {
-    window.location.href = "cart.html";
-}
-
-function cEstEmailValide() {
-    const email = form.email().value;
-    if (!email) {
-        return false;
+    if(bonEmailBomMotPasse()){
+        window.location.href = "cart.html";
+    } else {
+        alert("Nom d'utilisateur ou mot de passe invalide.");
     }
-    return valideEmail(email);
 }
+
+function bonEmailBomMotPasse() {
+    const email = form.email().value;
+    const motPasse = form.motPasse().value;
+    for (i = 0; i < listeUtilisateurs.length; i++) {
+        const emailDB = listeUtilisateurs[i].Email;
+        const motPasseDB = listeUtilisateurs[i].MotPasse;
+        if(email == emailDB && motPasse == motPasseDB) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function chargerUtilisateurs() {
+    var xhr;
+    try {
+      xhr = new ActiveXObject("Msxm12.XMLHTTP");
+    } catch (e) {
+      try {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e1) {
+        try {
+          xhr = new XMLHttpRequest();
+        } catch (e2) {}
+      }
+    }
+    xhr.open(
+      "GET",
+      "https://raw.githubusercontent.com/camimayer/Projet_web_2024/master/json/utilisateurs.json",
+      false
+    );
+    xhr.send();
+    if (xhr.readyState == 4) {
+      var reponse = xhr.responseText;
+      var objJSON = JSON.parse(reponse);
+      listeUtilisateurs = objJSON.Utilisateurs;
+    }
+  }
 
 function basculeErreursEmail() {
     const email = form.email().value;
-    form.emailRequis().style.display = email ? "none" : "block";
-
-    form.emailInvalide().style.display = valideEmail(email) ? "none" : "block";
+    if (!email) {
+        form.emailInvalide().style.display = "none";
+        form.emailRequis().style.display = "block";
+        form.br().style.display = "none";
+    } else if (!valideEmail(email)) {
+        form.emailRequis().style.display = "none";
+        form.emailInvalide().style.display = "block";
+        form.br().style.display = "none";
+    }else {
+        form.emailInvalide().style.display = "none";
+        form.emailRequis().style.display = "none";
+        form.br().style.display = "block";
+    }
 }
 
 function basculeErreursMotPasse() {
     const motPasse = form.motPasse().value;
-
-    form.motPasseRequis().style.display = motPasse ? "none" : "block";
-}
-
-function basculeBoutonsDesactives() {
-    const emailValide = cEstEmailValide();
-    form.motPasseOblie().disabled = !emailValide;
-
-    const motPasseValide = cEstMotPasseValide;
-    form.buttonContinue().disabled = !emailValide || !motPasseValide;
-}
-
-function cEstMotPasseValide() {
-    const motPasse = form.motPasse().value;
     if (!motPasse) {
-        return false;
+        form.motPasseRequis().style.display = "block";
+        form.br2().style.display = "none";
+    } else {
+        form.motPasseRequis().style.display = "none";
+        form.br2().style.display = "block";
     }
-    return true;
 }
 
 function valideEmail(email) {
@@ -57,8 +86,8 @@ const form = {
     email: () => document.getElementById("idEmail"),
     emailInvalide: () => document.getElementById("idEmailInvalide"),
     emailRequis: () => document.getElementById("idEmailRequis"),
-    buttonContinue: () => document.getElementById("idButtonContinue"),
     motPasse: () => document.getElementById("idMotPasse"),
     motPasseRequis: () => document.getElementById("idMotPasseRequis"),
-    motPasseOblie: () => document.getElementById("idButtonOblie")
+    br: () => document.getElementById("br"),
+    br2: () => document.getElementById("br2")
 }
